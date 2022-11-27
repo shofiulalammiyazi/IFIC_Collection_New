9a26f6fc6c7c25673bf6abc4686dd38320178d46
shaon82 <shaon82@users.noreply.github.com>
2022-11-27 11:56:26 +0600

    Delete target directory

--- target/classes/static/dashboard/custom/js/panel.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$("#gen").click(function(event) {
-		alert('hi');
-		$('#layout-form-general').removeClass('hide');
-		$('#layout-form-panel').removeClass('hide');
-		$('#layout-general').removeClass('hide');
-	});
-});
