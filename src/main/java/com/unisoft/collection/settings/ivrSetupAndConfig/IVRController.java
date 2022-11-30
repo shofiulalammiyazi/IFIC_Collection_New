fb8189fe35dd40d66330cc3ad1749de7e62f37bf
S M Junayed Shanto <S M Junayed Shanto>
2022-11-30 11:31:21 +0600

    ivr setup done

--- src/main/java/com/unisoft/collection/settings/ivrSetupAndConfig/IVRController.java
+++ src/main/java/com/unisoft/collection/settings/ivrSetupAndConfig/IVRController.java
@@ -1,13 +1,23 @@
 package com.unisoft.collection.settings.ivrSetupAndConfig;
 
 
+import com.unisoft.collection.samd.setup.riskCategories.RiskCategory;
+import com.unisoft.collection.settings.branch.Branch;
 import com.unisoft.collection.settings.employee.EmployeeService;
+import com.unisoft.user.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,9 +28,15 @@ public class IVRController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private IVRRepository ivrRepository;
+
 
     @GetMapping("/list")
     public String showList(Model model) {
+
+        model.addAttribute("ivrEntityList",ivrRepository.findAll());
+
         return "collection/settings/ivrSetup/list";
     }
 
@@ -30,10 +46,30 @@ public class IVRController {
         return createUpdateIvr(new IvrEntity(), model);
     }
 
-    private String createUpdateIvr(IvrEntity ivrEntity, Model model) {
+    @PostMapping("/create")
+    public String create(IvrEntity ivrEntity, Model model, BindingResult result) {
+
+        ivrRepository.save(ivrEntity);
+
+        return createUpdateIvr(ivrEntity, model);
+    }
+
+    @GetMapping("/edit")
+    public String editView(Model model, @RequestParam Long id) {
+        IvrEntity ivrEntity = ivrRepository.getOne(id);
         List<String> dealerPinList = employeeService.getDealerList().stream().map(t -> t.getPin()).collect(Collectors.toList());
         model.addAttribute("ivrEntity", ivrEntity);
-        model.addAttribute("dealerPinList",dealerPinList);
+        model.addAttribute("dealerPinList", dealerPinList);
+
         return "collection/settings/ivrSetup/create";
     }
+
+    private String createUpdateIvr(IvrEntity ivrEntity, Model model) {
+
+        List<String> dealerPinList = employeeService.getDealerList().stream().map(t -> t.getPin()).collect(Collectors.toList());
+        model.addAttribute("ivrEntity", ivrEntity);
+        model.addAttribute("dealerPinList", dealerPinList);
+        return "collection/settings/ivrSetup/create";
+    }
+
 }
--- src/main/java/com/unisoft/collection/settings/ivrSetupAndConfig/IVRService.java
+++ src/main/java/com/unisoft/collection/settings/ivrSetupAndConfig/IVRService.java
@@ -3,6 +3,49 @@ package com.unisoft.collection.settings.ivrSetupAndConfig;
 
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.Date;
+
 @Service
 public class IVRService {
+
+    public void call(String callId) throws Exception {
+        //            URL url = new URL("https://www.twilio.com/blog/5-ways-to-make-http-requests-in-java");
+//            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+//            httpURLConnection.setRequestMethod("GET");
+//            int res = httpURLConnection.getResponseCode();
+//            System.out.println(res);
+
+        String accId = "234556";
+        String ageId = "2222";
+        String mobileNo = "+8801737829182";
+        Date date = new Date();
+        System.out.println(date.getTime());
+
+        System.out.println(this.getMD5Value(accId, ageId, mobileNo, date.getTime()));
+    }
+
+
+    private String getMD5Value(String accId, String ageId, String mobileNo, Long milis) throws Exception {
+
+        String s = accId.concat(ageId).concat(mobileNo).concat(String.valueOf(milis));
+        System.out.println(s);
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+
+        byte[] message = md.digest(s.getBytes());
+
+        BigInteger no = new BigInteger(1, message);
+        String ss = no.toString(16);
+
+        System.out.println(ss.length());
+
+        while (ss.length() < 32) {
+            ss = "0" + ss;
+        }
+
+        return Base64.getEncoder().encodeToString(ss.getBytes());
+    }
 }
