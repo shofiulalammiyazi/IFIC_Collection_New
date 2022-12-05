43245f24aa08621bcf483e4caf5aaf87ffdd24ae
S M Junayed Shanto <S M Junayed Shanto>
2022-12-05 18:01:08 +0600

    sms & ivr on test

--- src/main/java/com/unisoft/collection/settings/ivrSetupAndConfig/IVRService.java
+++ src/main/java/com/unisoft/collection/settings/ivrSetupAndConfig/IVRService.java
@@ -22,52 +22,51 @@ public class IVRService {
     public void call(String callId) throws Exception {
 
         Gson gson = new Gson();
-        String accId = "234556";
-        String ageId = "2222";
-        String mobileNo = "+8801737829182";
+        String accId = "550153";
+        String ageId = "9001";
+        String mobileNo = "8801618811202";
         Date date = new Date();
         String time = String.valueOf(date.getTime());
-        String skillId = "";
-        String hashVal = this.getMD5Value(accId, ageId, mobileNo, date.getTime());
+        String skillId = "AG";
+        String shareKey = "46b7dc11c0c5292a753457881db298d2";
+        String hashVal = this.getMD5Value(accId, ageId, mobileNo, date.getTime(),shareKey);
 
         IvrDto ivrDto = new IvrDto(
                 accId,ageId,mobileNo,time,hashVal,skillId);
 
+        System.out.println(gson.toJson(ivrDto).getBytes());
+
         String requestString = Base64.getEncoder().encodeToString(gson.toJson(ivrDto).getBytes());
 
-        this.call(requestString);
+        this.callUrl(requestString);
     }
 
 
-    private String getMD5Value(String accId, String ageId, String mobileNo, Long milis) throws NoSuchAlgorithmException {
+    private String getMD5Value(String accId, String ageId, String mobileNo, Long milis, String shareKey) throws NoSuchAlgorithmException {
 
-        String s = accId.concat(ageId).concat(mobileNo).concat(String.valueOf(milis));
+        String s = accId.concat(ageId).concat(mobileNo).concat(String.valueOf(milis).concat(shareKey));
         System.out.println(s);
 
         MessageDigest md = MessageDigest.getInstance("MD5");
 
         byte[] message = md.digest(s.getBytes());
 
-        BigInteger no = new BigInteger(1, message);
-        String ss = no.toString(16);
+        //BigInteger no = new BigInteger(1, message);
+        //String ss = no.toString(16);
 
-        System.out.println(ss.length());
+        //System.out.println(ss.length());
 
-        while (ss.length() < 32) {
-            ss = "0" + ss;
-        }
+        //System.out.println(Base64.getEncoder().encodeToString(ss.getBytes()));
 
-        System.out.println(Base64.getEncoder().encodeToString(ss.getBytes()));
-
-        return Base64.getEncoder().encodeToString(ss.getBytes());
+        return Base64.getEncoder().encodeToString(message);
     }
 
     private String callUrl(String request) throws IOException {
-            URL url = new URL(callUrl+request);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-            int res = httpURLConnection.getResponseCode();
-            System.out.println(res);
+        URL url = new URL(callUrl+request);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        int res = httpURLConnection.getResponseCode();
+        System.out.println(res);
 
         return String.valueOf(res);
     }
