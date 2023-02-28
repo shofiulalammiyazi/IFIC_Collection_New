66879a3ec533d10ad5f9ce9c8922a2b4f89301d9
Shofiul alam <shofiulalam.miyazi@gmail.com>
2023-02-28 15:08:32 +0600

    loan

--- src/main/java/com/unisoft/retail/loan/dataEntry/CustomerUpdate/accountInformationRepository/AccountInformationRepository.java
+++ src/main/java/com/unisoft/retail/loan/dataEntry/CustomerUpdate/accountInformationRepository/AccountInformationRepository.java
@@ -361,4 +361,10 @@ public interface AccountInformationRepository extends JpaRepository<AccountInfor
             "                                              FROM SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY SADRE3 " +
             "                                              WHERE SADRE3.TYPE = 'Distribution')", nativeQuery = true)
     List<Tuple> finAllEligibleDistributionList(String accNo);
+
+
+
+
+
+
 }
