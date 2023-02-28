b5a8a0c51a5771a47b1bb0bd77247e3b6635d3ab
S M Junayed Shanto <S M Junayed Shanto>
2023-02-28 17:17:27 +0600

    allocated account list loan

--- src/main/java/com/unisoft/collection/distribution/loan/loanAccountDistribution/LoanAccountDistributionDto.java
+++ src/main/java/com/unisoft/collection/distribution/loan/loanAccountDistribution/LoanAccountDistributionDto.java
@@ -4,39 +4,22 @@ import lombok.Data;
 
 import javax.persistence.Tuple;
 import java.util.Date;
+import java.util.Objects;
 
 @Data
 public class LoanAccountDistributionDto {
-
-    private String customerId;
 
     private String accountNo;
 
     private String customerName;
 
-    private Date createdDate;
-
-    private String outstandingAmount;
-
-    private String minimumDueAmount;
-
-    private String location;
-
     private String dpdBucket;
-
-    private String ageCode;
 
     private String supervisorName;
 
     private String delaerPin;
 
     private String delaerName;
-
-    private String AgencyName;
-
-    private String branchName;
-
-    private String monirotingStatus;
     private String branchMnemonic;
     private String totalOutstanding;
     private String productCode;
@@ -47,31 +30,25 @@ public class LoanAccountDistributionDto {
     private String noOfInstallmentDue;
     private String loanCLStatus;
     private String mobile;
+    private String isSmsSent;
 
     public LoanAccountDistributionDto(Tuple t) {
-        this.customerId = customerId;
-        this.accountNo = accountNo;
-        this.customerName = customerName;
-        this.createdDate = createdDate;
-        this.outstandingAmount = outstandingAmount;
-        this.minimumDueAmount = minimumDueAmount;
-        this.location = location;
-        this.dpdBucket = dpdBucket;
-        this.ageCode = ageCode;
-        this.supervisorName = supervisorName;
-        this.delaerPin = delaerPin;
-        this.delaerName = delaerName;
-        this.branchName = branchName;
-        this.monirotingStatus = monirotingStatus;
-        this.branchMnemonic = branchMnemonic;
-        this.totalOutstanding = totalOutstanding;
-        this.productCode = productCode;
-        this.dealReference = dealReference;
-        this.overdue = overdue;
-        this.emiAmount = emiAmount;
-        this.nextEMIDate = nextEMIDate;
-        this.noOfInstallmentDue = noOfInstallmentDue;
-        this.loanCLStatus = loanCLStatus;
-        this.mobile = mobile;
+        this.accountNo = Objects.toString(t.get("LOANACNO"),"-").trim();
+        this.customerName = Objects.toString(t.get("CUSTOMER_NAME"),"-").trim();
+        this.dpdBucket = Objects.toString(t.get("DPD"),"-").trim();
+        this.supervisorName = Objects.toString(t.get("SUPERVISOR_NAME"),"-").trim();
+        this.delaerPin = Objects.toString(t.get("DEALER_PIN"),"-").trim();
+        this.delaerName = Objects.toString(t.get("DEALER_NAME"),"-").trim();
+        this.branchMnemonic = Objects.toString(t.get("BRANCH_MNEMONIC"),"-").trim();
+        this.totalOutstanding = Objects.toString(t.get("TOTAL_OUTSTANDING"),"-").trim();
+        this.productCode = Objects.toString(t.get("PRODUCT_CODE"),"-").trim();
+        this.dealReference = Objects.toString(t.get("DEAL_REFERENCE"),"-").trim();
+        this.overdue = Objects.toString(t.get("OVERDUE"),"-").trim();
+        this.emiAmount = Objects.toString(t.get("EMI_AMOUNT"),"-").trim();
+        this.nextEMIDate = Objects.toString(t.get("NEXTEMIDATE"),"-").trim();
+        this.noOfInstallmentDue = Objects.toString(t.get("NO_OF_INSTALLMENT_DUE"),"-").trim();
+        this.loanCLStatus = Objects.toString(t.get("LOANCLSTATUS"),"-").trim();
+        this.mobile = Objects.toString(t.get("MOBILE"),"-").trim();
+        this.isSmsSent = Objects.toString(t.get("IS_SMS_SENT"),"-").trim();
     }
 }
