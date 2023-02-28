1df5069d87babc66f58e79b5fe9796fe369ec44c
Shofiul alam <shofiulalam.miyazi@gmail.com>
2023-02-28 18:11:39 +0600

    distribution

--- src/main/java/com/unisoft/retail/loan/dataEntry/CustomerUpdate/accountInformationRepository/AccountInformationRepository.java
+++ src/main/java/com/unisoft/retail/loan/dataEntry/CustomerUpdate/accountInformationRepository/AccountInformationRepository.java
@@ -181,6 +181,32 @@ public interface AccountInformationRepository extends JpaRepository<AccountInfor
             "    SADRE3.TYPE = 'SMS')",nativeQuery = true)
     List<Tuple> finAllEligibleSmsList1(Pageable pageable);
 
+    @Query(value = "SELECT AIE.LOANACNO, AIE.CUSTOMER_NAME, AIE.MOBILE, AIE.BRANCH_MNEMONIC, " +
+            "       AIE.PRODUCT_CODE, AIE.DEAL_REFERENCE, AIE.TOTAL_OUTSTANDING, AIE.OVERDUE, " +
+            "       AIE.EMI_AMOUNT, AIE.NEXTEMIDATE, AIE.NO_OF_INSTALLMENT_DUE, AIE.LOANCLSTATUS " +
+            "FROM ACCOUNT_INFORMATION_ENTITY AIE " +
+            "WHERE AIE.IS_SMS_SENT = 'N' " +
+            "  AND AIE.NEXTEMIDATE = (SELECT to_char((SYSDATE+3),'YYYY-MM-DD') FROM dual) " +
+            "  AND AIE.EMI_AMOUNT IS NOT NULL " +
+            "  AND AIE.MOBILE IS NOT NULL " +
+            "  AND LTRIM(RTRIM(UPPER(AIE.LOANCLSTATUS))) " +
+            "        IN (SELECT DISTINCT LTRIM(RTRIM(UPPER(LSE.NAME))) " +
+            "            FROM SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY SADRE " +
+            "                   LEFT JOIN SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY_LOAN_STATUS_ENTITY SADRELSE " +
+            "                     ON SADRE.ID = SADRELSE.SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY_ID " +
+            "                   LEFT JOIN LOAN_STATUS_ENTITY LSE ON SADRELSE.LOAN_STATUS_ENTITY_ID = LSE.ID " +
+            "            WHERE LSE.ENABLED = 1 AND SADRE.TYPE = 'SMS') " +
+            "  AND LTRIM(RTRIM(UPPER(AIE.PRODUCT_CODE))) " +
+            "        IN(SELECT DISTINCT LTRIM(RTRIM(UPPER(LTE.LOAN_TYPE))) " +
+            "           FROM SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY SADRE1 " +
+            "                  LEFT JOIN SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY_LOAN_TYPE_ENTITIES SADRELTE " +
+            "                    ON SADRE1.ID = SADRELTE.SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY_ID " +
+            "                  LEFT JOIN LOAN_TYPE_ENTITY LTE ON SADRELTE.LOAN_TYPE_ENTITIES_ID = LTE.ID " +
+            "           WHERE LTE.ENABLED = 1 AND SADRE1.TYPE = 'SMS') " +
+            "  AND TO_NUMBER(AIE.NO_OF_INSTALLMENT_DUE) < (SELECT TO_NUMBER(SADRE3.UNPAID_INSTALLMENT_NUMBER) FROM SMS_AND_AUTO_DISTRIBUTION_RULES_ENTITY SADRE3 WHERE " +
+            "    SADRE3.TYPE = 'SMS')",nativeQuery = true)
+    List<Tuple> finAllEligibleSmsList1();
+
     @Query(value = "SELECT COUNT(AIE.LOANACNO) "+
             "FROM ACCOUNT_INFORMATION_ENTITY AIE " +
             "WHERE AIE.IS_SMS_SENT = 'N' " +
