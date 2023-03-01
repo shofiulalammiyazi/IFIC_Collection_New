e448d34676639044c2637df46d26257d9c2eb8b4
Shofiul alam <shofiulalam.miyazi@gmail.com>
2023-03-01 18:02:53 +0600

    new user

--- src/main/java/com/unisoft/userInformation/UserInformationController.java
+++ src/main/java/com/unisoft/userInformation/UserInformationController.java
@@ -1,5 +1,9 @@
 package com.unisoft.userInformation;
 
+import com.unisoft.collection.settings.employee.API.EmployeeDetails;
+import com.unisoft.collection.settings.employee.EmployeeInfoEntity;
+import com.unisoft.role.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/userInformation/")
 public class UserInformationController {
 
+    @Autowired
+    private RoleService roleService;
+
     @GetMapping(value = "list")
     public String list(Model model){
 
@@ -17,6 +24,9 @@ public class UserInformationController {
 
     @GetMapping(value = "create")
     public String create(Model model){
+
+        model.addAttribute("entity", new EmployeeDetails());
+        model.addAttribute("roles", roleService.getActiveList());
 
      return "/card/contents/settings/userInformation/create";
     }
